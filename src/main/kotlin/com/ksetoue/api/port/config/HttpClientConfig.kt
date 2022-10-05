942b16d3f2b8cc56eff99bb292cbf9da83af2f0f
ksetoue <karoline.kimiko@gmail.com>
2022-10-04 22:21:37 -0300

    :wrench: add CI

---
+++ src/main/kotlin/com/ksetoue/api/port/config/HttpClientConfig.kt
@@ -0,0 +1,13 @@
+package com.ksetoue.api.port.config
+
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.client.RestTemplate
+
+@Configuration
+class HttpClientConfig {
+    @Bean
+    fun restClient(restTemplateBuilder: RestTemplateBuilder): RestTemplate =
+        restTemplateBuilder.build()
+}
