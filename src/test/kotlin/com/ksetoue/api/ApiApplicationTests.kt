cb8f1d5f89dc456833c6bd6621836b1f09d667dc
ksetoue <karoline.kimiko@gmail.com>
2022-10-21 18:43:18 -0300

    :wrench: fix test context load

--- src/test/kotlin/com/ksetoue/api/ApiApplicationTests.kt
+++ src/test/kotlin/com/ksetoue/api/ApiApplicationTests.kt
@@ -1,12 +1,12 @@
 package com.ksetoue.api
 
-import org.junit.jupiter.api.Test
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
+@DataJpaTest
 class ApiApplicationTests {
 
-    @Test
     fun contextLoads() {
     }
 }
